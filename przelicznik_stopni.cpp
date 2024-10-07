6f9bec369aeb3647c5980f15569f57d197b3ab21
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-07 20:39:32 +0200

    Update przelicznik_stopni.cpp
    Program zmieniony na wersję v0.2. Już nie wywala po wpisaniu litery w miejsce liczby.

--- przelicznik_stopni.cpp
+++ przelicznik_stopni.cpp
@@ -21,11 +21,11 @@ void logo()
 `--'     `--'   `-----' `----'`--'`--' `---'`-----'`--''--'`--'`--'`--'
 
 
-            ,--.                        ,--.               ,--.     ,--.
-     ,---.,-'  '-. ,---.  ,---. ,--,--, `--'    ,--.  ,--./    \   /   |
-    (  .-''-.  .-'| .-. || .-. ||      \,--.     \  `'  /|  ()  |  `|  |
-    .-'  `) |  |  ' '-' '| '-' '|  ||  ||  |      \    /  \    /.--.|  |
-    `----'  `--'   `---' |  |-' `--''--'`--'       `--'    `--' '--'`--'
+            ,--.                        ,--.               ,--.      ,---.
+     ,---.,-'  '-. ,---.  ,---. ,--,--, `--'    ,--.  ,--./    \    '.-.  \
+    (  .-''-.  .-'| .-. || .-. ||      \,--.     \  `'  /|  ()  |    .-' .'
+    .-'  `) |  |  ' '-' '| '-' '|  ||  ||  |      \    /  \    /.--./   '-.
+    `----'  `--'   `---' |  |-' `--''--'`--'       `--'    `--' '--''-----'
                          `--'                                               )";
     cout<<"\n Zbudowany przez @AndreansxTech na Github\n";
     int linia=0;
@@ -42,7 +42,6 @@ int main()
     logo();
     double k,f,c=0.0;
     char wybor;
-
         while (true)
         {
         cout<<"\n";informacja();
@@ -52,21 +51,45 @@ int main()
         {
         case 'c':
         case 'C':
-            informacja(); cout<<" Wybrales Celsjusze\n Wprowadz wartosc: "; cin>>c;
+            informacja(); cout<<" Wybrales Celsjusze\n Wprowadz wartosc: ";
+            while(!(cin>>c))
+            {
+                informacja();
+                cout<<" Nie poprawna liczba\n"; SetConsoleTextAttribute(color,11);
+                cout<<" Wprowadz wartosc w Celsjuszach jeszcze raz: "; cin>>c;
+                cin.clear();
+                cin.ignore();
+            }
             f=(2*(c-0.1*c))+32; k=c+273.15; SetConsoleTextAttribute(color,11); cout<<"\n";
-            cout<<c<<" stopni Celsjusza to:\n"<<f<<" stopni Fahrenheita\n"<<k<<" Kelwinow\n";
+            cout<<c<<" stopni Celsjusza to:\n"<<f<<" stopni Fahrenheita\n oraz\n"<<k<<" Kelwinow\n";
             break;
         case 'F':
         case 'f':
-            informacja(); cout<<" Wybrales Fahrenheity\n Wprowadz wartosc: "; cin>>f;
+            informacja(); cout<<" Wybrales Fahrenheity\n Wprowadz wartosc: ";
+            while(!(cin>>f))
+            {
+                informacja();
+                cout<<" Nie poprawna liczba\n"; SetConsoleTextAttribute(color,11);
+                cout<<" Wprowadz wartosc w Fahrenheitach jeszcze raz: "; cin>>f;
+                cin.clear();
+                cin.ignore();
+            }
             c=(5.0/9.0)*(f-32); k=(c+273.15);cout<<"\n";SetConsoleTextAttribute(color,11);
-            cout<<f<<" stopni fahrenheita to:\n"<<c<<" stopni Celsjusza\n"<<k<<" Kelwinow\n";
+            cout<<f<<" stopni fahrenheita to:\n"<<c<<" stopni Celsjusza\n oraz\n"<<k<<" Kelwinow\n";
             break;
         case 'K':
         case 'k':
-            informacja(); cout<<" Wybrales Kelwiny\n Wprowadz wartosc: ";cin>>k;
-            c=k-273.15; f=f=(2*(c-0.1*c))+32;cout<<"\n"; SetConsoleTextAttribute(color,11);
-            cout<<k<<" Kelwinow to: \n"<<c<<" stopni Celsjusza\n"<<f<<" stopni Fahrenheita\n";
+            informacja(); cout<<" Wybrales Kelwiny\n Wprowadz wartosc: ";
+            while(!(cin>>k))
+            {
+                informacja();
+                cout<<" Nie poprawna liczba\n"; SetConsoleTextAttribute(color,11);
+                cout<<" Wprowadz wartosc w Kelwinach jeszcze raz: "; cin>>k;
+                cin.clear();
+                cin.ignore();
+            }
+            c=k-273.15; f=(2*(c-0.1*c))+32;cout<<"\n"; SetConsoleTextAttribute(color,11);
+            cout<<k<<" Kelwinow to: \n"<<c<<" stopni Celsjusza\n oraz\n"<<f<<" stopni Fahrenheita\n";
             break;
         case 'W':
         case 'w':
